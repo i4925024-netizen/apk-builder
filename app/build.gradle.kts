de92994a84129aa3344df1fefb26f814c1710071
i4925024-netizen <i4925024@gmail.com>
2026-08-30 10:26:57 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "com.future.htmlbuilder"; compileSdk = 35
-    defaultConfig { applicationId = "com.future.htmlbuilder"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+    defaultConfig { compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+} }
     buildTypes { release { isMinifyEnabled = false; proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro") } }
 }
 
